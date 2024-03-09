4e0037da2334bcc741f91e9c411f5ce6624ff611
menna-adel-10 <menna10_adel@hotmail.com>
2024-03-10 00:22:29 +0200

    .

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,8 +1,20 @@
+import HeroPng from "../../assets/0_3.webp"
 import { BiPlayCircle } from "react-icons/bi";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <>
+      <div className="py-12 sm:py-0 dark:bg-black dark:text-white duration-300 overflow-hidden">
+        <div className="container min-h-[700px] flex relative">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+
+            <div>
+              <img src={HeroPng} alt="" className="rounded-full" width={500} height={500} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
