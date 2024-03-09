260dd2860bb8a2e31ef60f69bce08a71640d4036
menna-adel-10 <menna10_adel@hotmail.com>
2024-03-09 17:54:46 +0200

    navbar settle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"]
+      },
+      colors: {
+        primary: "#00d1de",
+        secondary: "#15ed3c",
+        dark: "#111111",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        },
+      },
+    },
   },
   plugins: [],
 }
-
